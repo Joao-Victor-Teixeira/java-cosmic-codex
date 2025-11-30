de324b2feca6d83adf6fe390ef2ee9e5d1357f0d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-30 11:36:25 -0300

    Cria projeto com as suas dependências primarias
    BIG BANG do projeto

---
+++ cosmic-codex/src/main/java/com/joaodev/cosmic_codex/CosmicCodexApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.cosmic_codex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicCodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicCodexApplication.class, args);
+	}
+
+}
---
+++ cosmic-codex/src/test/java/com/joaodev/cosmic_codex/CosmicCodexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.cosmic_codex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmicCodexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
